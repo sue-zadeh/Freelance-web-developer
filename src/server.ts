3bdae04a34c584d607543798e6b871206e884662
Sue Raisianzadeh <raisianz@gmail.com>
2026-04-29 02:59:30 +1200

    npm build

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const pool = mysql.createPool({
   host: process.env['DB_HOST'] || 'localhost',
   user: process.env['DB_USER'] || 'freelance_user',
   password: process.env['DB_PASSWORD'] || '',
-  database: process.env['DB_NAME'] || 'freelance',
+  database: process.env['DB_NAME'] || '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
